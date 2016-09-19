ba75b7f51a283685e6cd406d9fa8ac71634aefd3
LeeBox <li_xiaoliang@foxmail.com>
2016-09-19 18:58:49 +0800

    修改颜色

--- src/components/accordion/example.vue
+++ src/components/accordion/example.vue
@@ -2,31 +2,31 @@
 
 <accordion :auto-shrink="true">
     <accordion-item title="1.SHIPPING">
-        <div style="height: 400px;width: 100%;background: yellow">
-            asd
+        <div style="height: 400px;width: 100%;background: #faca21">
+            这是一个height 400px的div
         </div>
-        <div style="height: 400px;width: 100%;background: green">
-            zxczxc
+        <div style="height: 400px;width: 100%;background: #1aca21">
+            这是一个height 400px的div
         </div>
     </accordion-item>
     <accordion-item title="2.BILLING">
-        <div style="height: 400px;width: 100%;background: red">
-            zxcz阿斯达xc
+        <div style="height: 400px;width: 100%;background: #aaca21">
+            这是一个height 400px的div
         </div>
     </accordion-item>
     <accordion-item title="3.REVIEW">
-        <div style="height: 200px;width: 100%;background: red">
-            zxczxllxlxlxlxlxlxl
+        <div style="height: 200px;width: 100%;background: pink">
+            这是一个height 200px的div
         </div>
     </accordion-item>
     <accordion-item title="3.TEST">
-        <div style="height: 400px;width: 100%;background: red">
-            zxcz阿斯达xc
+        <div style="height: 400px;width: 100%;background: #aaca21">
+            这是一个height 400px的div
         </div>
     </accordion-item>
 </accordion>
-<div style="background-color:yellow;height:200px;">
-
+<div style="background-color:black;height:200px;color:#fff;text-align:center;line-height:200px;">
+  这是内容
 </div>
 </template>
 
